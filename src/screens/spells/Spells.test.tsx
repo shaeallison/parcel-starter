522246de489fe8ecadee80d20a1fff5f59c49651
Shae Allison <shae.allison7@gmail.com>
2024-03-13 10:33:55 -0400

    fixing tests

--- src/screens/spells/Spells.test.tsx
+++ src/screens/spells/Spells.test.tsx
@@ -55,44 +55,17 @@ const testSpells: SpellDataType[] = [
 ]
 
 it('should return all spells if there are no applied filters', () => {
-  expect(narrowSpells(testSpells, null, null, null)).toEqual(testSpells)
+  expect(narrowSpells(testSpells, null)).toEqual(testSpells)
 })
 
 it('should narrow to single result with high specificity substring', () => {
-  expect(narrowSpells(testSpells, 'Bombarda', null, null)).toEqual([
-    testSpells[1]
-  ])
+  expect(narrowSpells(testSpells, 'Bombarda')).toEqual([testSpells[1]])
 })
 
 it('should narrow to multiple results with low specificity substring', () => {
-  expect(narrowSpells(testSpells, 'a', null, null)).toEqual([
-    testSpells[0],
-    testSpells[1]
-  ])
+  expect(narrowSpells(testSpells, 'a')).toEqual([testSpells[0], testSpells[1]])
 })
 
 it('should return no spells when no names match', () => {
-  expect(narrowSpells(testSpells, 'test', null, null)).toEqual([])
-})
-
-it('should return spells with damage greater than or equal to 10', () => {
-  expect(narrowSpells(testSpells, null, '10', null)).toEqual([
-    testSpells[1],
-    testSpells[3]
-  ])
-})
-
-it('should return spells with letter "c" AND coolDown less than or equal to 10', () => {
-  expect(narrowSpells(testSpells, 'c', null, '10')).toEqual([
-    testSpells[0],
-    testSpells[3]
-  ])
-})
-
-it('should return spells with damage greater than "20" and coolDown less than or equal to "15', () => {
-  expect(narrowSpells(testSpells, null, '20', '15')).toEqual([testSpells[1]])
-})
-
-it('should return spells containing substring "bom" damage greater than "20" and coolDown less than or equal to "15', () => {
-  expect(narrowSpells(testSpells, null, '20', '15')).toEqual([testSpells[1]])
+  expect(narrowSpells(testSpells, 'test')).toEqual([])
 })
